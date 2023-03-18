10ee78ba1e9e11658048b67bab9f4dc0766d548d
Santiago Junco <77699351+santi-junco@users.noreply.github.com>
2023-03-17 22:16:19 -0300

    Configuraciones basicas

---
+++ santiDeportes/settings/dev.py
@@ -0,0 +1,28 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
+
+# Database
+# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': parser.get('default','name'),
+        'USER':  parser.get('default','user'),
+        'PASSWORD':  parser.get('default','password'),
+        'HOST': parser.get('default','host'),
+        'PORT': '5432'
+    }
+}
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = parser.get('email','user')
+EMAIL_HOST_PASSWORD = parser.get('email','password')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
