92f9505a470d6da300765547403bc868e32679c0
Santiago Junco <77699351+santi-junco@users.noreply.github.com>
2023-03-19 02:28:56 -0300

    Envio de email para verificacion, no anda la redireccion del boton

---
+++ apps/core/email.py
@@ -0,0 +1,21 @@
+from django.template.loader import render_to_string
+from django.core.mail import send_mail
+from django.conf import settings
+
+def sendMail(tipo, asunto, destino, url):
+    
+    context = {
+        'tipo': tipo,
+        'url': url
+    }
+
+    html = render_to_string('email.html', context)
+
+    from_email = settings.EMAIL_HOST_USER
+    recipient_list = [destino,]
+    subject = asunto
+    message = 'mensaje'
+
+    send_mail(subject, message, from_email, recipient_list, html_message=html)
+
+    return True
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+from django.db import transaction
+
+from apps.core.email import sendMail
 
 from .models import Usuario
 from .forms import UserForm
@@ -10,3 +13,13 @@ class UserCreate(CreateView):
     form_class = UserForm
     template_name = 'signup.html'
     success_url = reverse_lazy('home')
+
+    def form_valid(self, form):
+        with transaction.atomic():
+            response = super().form_valid(form)
+            user = self.object
+            url = f'127.0.0.1:8000/users/verification/{user.id}/'
+            sendMail('registro', 'Registro Exitoso', user.email, url)
+            
+            return response
+
