bf1ff18794fd8a9c5ccd021f380f0ffafb42ee3a
Santiago Junco <77699351+santi-junco@users.noreply.github.com>
2023-03-19 00:26:15 -0300

    mostrar productos

--- santiDeportes/urls.py
+++ santiDeportes/urls.py
@@ -14,6 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 
 urlpatterns = [
@@ -22,3 +25,7 @@ urlpatterns = [
     path('', include('apps.products.urls')),
     path('users/', include('apps.users.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += staticfiles_urlpatterns()
